f6790bb771498f51b04a0400cf830f33f2ca61de
raksha-rajpoot <rakraj654@gmail.com>
2024-12-25 20:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+   const minHeap = new MinHeap();
+    arr.forEach(length => minHeap.insert(length));
+    
+    let totalCost = 0;
+    
+    while (minHeap.size() > 1) {
+        const first = minHeap.extractMin();
+        const second = minHeap.extractMin();
+        
+        const cost = first + second;
+        totalCost += cost;
+        
+        minHeap.insert(cost);
+    }
+    
+    return totalCost;
 }
 
 module.exports=mincost;
