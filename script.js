44ab82eabbd98213ffef78835e401f396a615a59
raksha-rajpoot <rakraj654@gmail.com>
2024-12-25 20:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-   const minHeap = new MinHeap();
-    arr.forEach(length => minHeap.insert(length));
-    
+    const minHeap = [...arr].sort((a, b) => a - b); // Sort the array to mimic heap behavior
+
     let totalCost = 0;
-    
-    while (minHeap.size() > 1) {
-        const first = minHeap.extractMin();
-        const second = minHeap.extractMin();
-        
+
+    while (minHeap.length > 1) {
+        const first = minHeap.shift();
+        const second = minHeap.shift();
+
         const cost = first + second;
         totalCost += cost;
-        
-        minHeap.insert(cost);
+
+        const index = minHeap.findIndex(x => x > cost);
+        if (index === -1) {
+            minHeap.push(cost);
+        } else {
+            minHeap.splice(index, 0, cost);
+        }
     }
-    
+
     return totalCost;
 }
 
